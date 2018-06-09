012cc9260a2d097cec82e7d3e4d8224dad491d32
myvagrantlife <edward@myvagrantlife.com>
2018-06-09 21:51:36 +0200

    display styled orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,24 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = (props) => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+  const ingredientOutput = ingredients.map(ig => {
+    return <span
+            key={ig.name}
+            className={classes.Ingredient}>
+              {ig.name} ({ig.amount})
+            </span>
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>$5.45</strong></p>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
   );
 };
