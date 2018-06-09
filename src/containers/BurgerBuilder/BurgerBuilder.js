e01e2d697f3ec33bd3664bc9a8c3341e21dabbb2
myvagrantlife <edward@myvagrantlife.com>
2018-06-09 20:30:12 +0200

    handler order now and cancel buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,27 +87,28 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({loading: true});
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice, // don't send price in real app
-      customer: {
-        name: 'Edward Smith',
-        address: {
-          street: 'Test Street',
-          postCode: 'AHGSJD',
-          country: 'UK'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({loading:false, purchasing: false});
-      }).catch(error => {
-        this.setState({loading:false, purchasing: false});
-      });
+    // this.setState({loading: true});
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice, // don't send price in real app
+    //   customer: {
+    //     name: 'Edward Smith',
+    //     address: {
+    //       street: 'Test Street',
+    //       postCode: 'AHGSJD',
+    //       country: 'UK'
+    //     },
+    //     email: 'test@test.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({loading:false, purchasing: false});
+    //   }).catch(error => {
+    //     this.setState({loading:false, purchasing: false});
+    //   });
+    this.props.history.push('/checkout');
   }
 
   render () {
